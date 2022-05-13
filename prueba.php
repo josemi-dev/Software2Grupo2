36d1f6ffd8fb4aeaf3060992ba595f2e549cbd56
josemi-dev <jomigu3@gmail.com>
2022-05-13 13:07:32 -0400

    primera prueba

---
+++ prueba.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TAREA 5</title>
+    
+</head>
+<body>
+    <H1>PRUEBA EN GITHUB DE LA TAREA N° 5</H1>
+    <H2>INGENIERÍA DE SOFTWARE 2</H2>
+    <H3>GRUPO N° 2</H3>
+    <H4>INTEGRANTES:</H4>
+    <P>JOSÉ MILTON GUARDIA MAMANI</P>
+    
+</body>
+</html>
